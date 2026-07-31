92cc058f61afdb6107d0793f3953ed0a8ad54daf
Abdulrahman <abdulrahmanhendieh@gmail.com>
2026-07-31 14:35:03 +0200

    rate limiter in api enpoints

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from core.database import Base, engine 
+from core.api_limiter import limiter
 from routers.user_routers import user_api_router
+from slowapi import  _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 
 
 Base.metadata.create_all(bind = engine) #createing all tables 
 
 app = FastAPI()
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+
 app.include_router(user_api_router)
 @app.get('/')
 def home():
--- backend/routers/user_routers.py
+++ backend/routers/user_routers.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from schemas.user_schemas import UserOut, CreateUser, Token, Authenticate_data, RefreshToken
 from sqlalchemy.orm import session
 from core.database import get_db
 from crud.user_crud import *
+from core.api_limiter import limiter
 
 
 
@@ -10,17 +11,20 @@ user_api_router = APIRouter(prefix = "/user")
 
 
 @user_api_router.post("/create", response_model = UserOut)
-def create_user(user: CreateUser, db: session = Depends(get_db)):
+@limiter.limit("5/minute")
+def create_user(request: Request  ,user: CreateUser, db: session = Depends(get_db)):
     return register_user(db, user)
 
 
 @user_api_router.post("/login", response_model = Token)
-def login_user(user: Authenticate_data, db: session = Depends(get_db)):
+@limiter.limit("5/minute")
+def login_user(request: Request, user: Authenticate_data, db: session = Depends(get_db)):
     return authenticate_user(user, db)
 
 
 @user_api_router.delete("/delete")
-def remove_my_account(username: str, current_user: User = Depends(get_current_user), db: session = Depends(get_db)):
+@limiter.limit("5/minute")
+def remove_my_account(request: Request, username: str, current_user: User = Depends(get_current_user), db: session = Depends(get_db)):
     
     if current_user.username != username:
         raise HTTPException(
@@ -31,10 +35,12 @@ def remove_my_account(username: str, current_user: User = Depends(get_current_us
     return delete_my_account(username = username, db = db)
 
 @user_api_router.post("/logout")
-def logout_user(token: str = Depends(settings.oauth2_scheme),db: session = Depends(get_db)):
+@limiter.limit("5/minute")
+def logout_user(request: Request, token: str = Depends(settings.oauth2_scheme),db: session = Depends(get_db)):
     return logout(token = token, db = db)
 
 
 @user_api_router.post("/refresh", response_model = Token)
-def refresh_user_access_token(data: RefreshToken, db: session = Depends(get_db)):
+@limiter.limit("5/minute")
+def refresh_user_access_token(request: Request, data: RefreshToken, db: session = Depends(get_db)):
     return refresh_access_token(data = data, db = db)
